6bdac4b110d50150bd8f370cbfc0549dbbc66c46
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-10-30 04:00:26 -0300

    act

--- contactos/forms.py
+++ contactos/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from .models import Contacto
+import re
 
 class ContactoForm(forms.ModelForm):
     class Meta:
         model = Contacto
         fields = ['nombre', 'telefono', 'correo', 'direccion']
+
+    def clean_telefono(self):
+        telefono = self.cleaned_data['telefono'].replace(" ", "")
+        # Permitir +56 o nada al inicio
+        if telefono.startswith("+56"):
+            telefono = telefono[3:]
+        if telefono.startswith("9") and len(telefono) == 9 and telefono.isdigit():
+            return self.cleaned_data['telefono']
+        elif len(telefono) == 9 and telefono.isdigit():
+            return self.cleaned_data['telefono']
+        raise forms.ValidationError("El teléfono debe ser chileno, empezar con 9 y tener 9 dígitos (Ej: 912345678 o +56 912345678).")
--- contactos/models.py
+++ contactos/models.py
@@ -1,10 +1,24 @@
 from django.db import models
-from django.core.validators import EmailValidator
+from django.core.validators import RegexValidator, EmailValidator
+
+telefono_validator = RegexValidator(
+    regex=r'^(\+56\s?)?9\d{8}$',
+    message='Ingrese un número de teléfono chileno válido. Ejemplo: +56 912345678 o 912345678'
+)
+
+email_validator = EmailValidator(
+    message='Ingrese un correo electrónico válido.'
+)
 
 class Contacto(models.Model):
     nombre = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=20)
-    correo = models.EmailField(validators=[EmailValidator()])
+    telefono = models.CharField(
+        max_length=15,
+        validators=[telefono_validator]
+    )
+    correo = models.EmailField(
+        validators=[email_validator]
+    )
     direccion = models.CharField(max_length=255)
 
     def __str__(self):
