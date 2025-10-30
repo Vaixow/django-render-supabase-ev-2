8f905410c8b739f7bef05d173c0d16407f02c680
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-10-30 04:06:20 -0300

    Update admin.py

--- contactos/admin.py
+++ contactos/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Contacto
 
-admin.site.register(Contacto)
+@admin.register(Contacto)
+class ContactoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "telefono", "correo", "direccion")
+    search_fields = ("nombre", "telefono", "correo")
+    list_filter = ("correo",)
+    ordering = ("nombre",)
+    list_per_page = 25
