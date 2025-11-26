3662fd2ec7b638912c50b4da888d8234f983099a
Vicente Alcaíno <vicente.alcaino@inacapmail.cl>
2025-11-26 18:06:25 -0300

    feat

--- agenda_contactos/settings.py
+++ agenda_contactos/settings.py
@@ -49,11 +49,15 @@ ALLOWED_HOSTS = [
 ]
 ##DESDE SITIOS ESPECIFICOS
 CORS_ALLOWED_ORIGINS = [
-    "https://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://localhost:5500",
+    "http://localhost:4200",
+    "http://127.0.0.1:8000",
+    "https://ultimo-intento-ahora-si.onrender.com",
 ]
 ### PERMITIR CORS DESDE TODAS PARTES (solo desarrollo)
-CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Application definition
